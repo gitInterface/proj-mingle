44f524409f4c85445747099205a6d5b4ab914035
aura70001 <b0232036@ems.niu.edu.tw>
2024-01-12 17:01:07 +0800

    import the repository of spring-boot-websocket and add config

--- src/main/java/com/ispan/mingle/projmingle/Service/ChatAjaxService.java
+++ src/main/java/com/ispan/mingle/projmingle/Service/ChatAjaxService.java
@@ -1,8 +1,11 @@
 package com.ispan.mingle.projmingle.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ispan.mingle.projmingle.domain.MessageBean;
 import com.ispan.mingle.projmingle.repository.ChatRepository;
 
 import jakarta.transaction.Transactional;
@@ -12,4 +15,8 @@ import jakarta.transaction.Transactional;
 public class ChatAjaxService {
     @Autowired
     private ChatRepository chatRepository = null;
+
+    public List<MessageBean> getChatHistory(String recieverID, String serderID) {
+        return null;
+    }
 }
--- src/main/java/com/ispan/mingle/projmingle/controller/ChatController.java
+++ src/main/java/com/ispan/mingle/projmingle/controller/ChatController.java
@@ -1,8 +1,15 @@
 package com.ispan.mingle.projmingle.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class ChatController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greeting")
+    public void testRecieved() {
+
+    }
 
 }
