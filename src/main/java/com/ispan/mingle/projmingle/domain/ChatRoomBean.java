4cb3049b3d0c19fd03ec183dfa159676a63d7215
aura70001 <b0232036@ems.niu.edu.tw>
2024-01-19 14:22:13 +0800

    solve problem about creating chatroom

--- src/main/java/com/ispan/mingle/projmingle/domain/ChatRoomBean.java
+++ src/main/java/com/ispan/mingle/projmingle/domain/ChatRoomBean.java
@@ -2,6 +2,8 @@ package com.ispan.mingle.projmingle.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 public class ChatRoomBean {
     @Id
     @Column(name = "roomID", columnDefinition = "int")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer roomid;
     @Column(name = "chatID", columnDefinition = "varchar", length = 100)
     private String chatid;
