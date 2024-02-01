f7d6d7975f084fbad4bf147bba9253d3bd5f55f5
aura70001 <b0232036@ems.niu.edu.tw>
2024-02-01 14:51:38 +0800

    MessageBean新增欄位 (type)

--- src/main/java/com/ispan/mingle/projmingle/domain/MessageBean.java
+++ src/main/java/com/ispan/mingle/projmingle/domain/MessageBean.java
@@ -52,4 +52,6 @@ public class MessageBean {
 
     @Column(name = "chatID", columnDefinition = "varchar(100)")
     private String chatid;
+    @Column(name = "type", columnDefinition = "int")
+    private Integer type;
 }
