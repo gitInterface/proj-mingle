5fcf50bd2948f54a59dbfbe14564a54e009bf0a7
Cindy <onlyyu147@gmail.com>
2024-02-16 04:14:04 +0800

    新增數據分析所需後端

--- src/main/java/com/ispan/mingle/projmingle/repository/LandlordRepository.java
+++ src/main/java/com/ispan/mingle/projmingle/repository/LandlordRepository.java
@@ -1,7 +1,7 @@
 package com.ispan.mingle.projmingle.repository;
 
-import com.ispan.mingle.projmingle.dto.LandlordOrderDTO;
-import org.hibernate.query.Order;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Repository;
 import com.ispan.mingle.projmingle.domain.LandlordBean;
 import com.ispan.mingle.projmingle.domain.OrderBean;
 import com.ispan.mingle.projmingle.domain.ReviewBean;
-
-import java.util.List;
+import com.ispan.mingle.projmingle.domain.WorkBean;
+import com.ispan.mingle.projmingle.dto.LandlordOrderDTO;
 
 @Repository
 public interface LandlordRepository extends JpaRepository<LandlordBean, Integer> {
@@ -57,16 +57,106 @@ public interface LandlordRepository extends JpaRepository<LandlordBean, Integer>
                         "WHERE o.orderid = :id ")
         void setOrderStatus(Integer id, String status, boolean cancelled);
 
+        @Query("SELECT new com.ispan.mingle.projmingle.dto.LandlordOrderDTO(w.name, h.name, o) " +
+                        "FROM OrderBean o " +
+                        "JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "JOIN WorkBean w ON w.workid = wh.workid " +
+                        "JOIN LandlordBean l ON w.landlordid = l.landlordid " +
+                        "JOIN HouseBean h ON h.houseid = wh.houseid " +
+                        "WHERE o.orderid = :id")
+        List<LandlordOrderDTO> findByOrderid(Integer id);
 
-    @Query("SELECT new com.ispan.mingle.projmingle.dto.LandlordOrderDTO(w.name, h.name, o) " +
-            "FROM OrderBean o " +
-            "JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
-            "JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
-            "JOIN WorkBean w ON w.workid = wh.workid " +
-            "JOIN LandlordBean l ON w.landlordid = l.landlordid " +
-            "JOIN HouseBean h ON h.houseid = wh.houseid " +
-            "WHERE o.orderid = :id")
-    List<LandlordOrderDTO> findByOrderid(Integer id);
+        /** 透過房東id查詢工作 */
+        @Query("SELECT w FROM OrderBean o " +
+                        "JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId")
+        public List<WorkBean> findWorkByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 透過房東id查詢房東擁有的訂單數 */
+        @Query("SELECT COUNT(o) FROM OrderBean o " +
+                        "JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId")
+        public Integer findTotalNumberByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 透過房東id查詢房東擁有的訂單報名人數 */
+        @Query("SELECT SUM(o.numbers) FROM OrderBean o " +
+                        "JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId")
+        public Integer findOrderNumberByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 透過房東id查詢房東每年每月訂單數 */
+        @Query("SELECT YEAR(o.createdAt) AS order_year, " +
+                        "MONTH(o.createdAt) AS order_month, " +
+                        "COUNT(o) AS monthly_order_count " +
+                        "FROM OrderBean o " +
+                        "JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId " +
+                        "GROUP BY YEAR(o.createdAt), MONTH(o.createdAt)")
+        public List<Object[]> findMonthlyOrderCountByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 透過房東id查詢房東每個工作訂單數 */
+
+        @Query("SELECT COUNT(o), w.workid, w.name " +
+                        "FROM OrderBean o " +
+                        "full JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "full JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "full JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId " +
+                        "GROUP BY w.workid, w.name")
+        public List<Object[]> findWorkOrderCountByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 透過房東id查詢所有工作瀏覽量總和 */
+
+        @Query("SELECT SUM(w.views) FROM OrderBean o " +
+                        "full JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "full JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "full JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId")
+        public Integer findTotalViewsByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 透過房東id查詢每個工作的瀏覽量 */
+        @Query("SELECT w.workid ,w.views, w.name FROM OrderBean o " +
+                        "full JOIN OrderWorkHouseBean owh ON o.orderid = owh.orderid " +
+                        "full JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "full JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId GROUP BY w.workid ,w.views, w.name")
+        public List<Object[]> findWorkViewsByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 透過房東id查詢對房東的平均評價分數 */
+
+        @Query("SELECT AVG(r.stars)  FROM OrderBean o " +
+                        "full JOIN ReviewBean r ON r.orderid = o.orderid " +
+                        "full JOIN OrderWorkHouseBean owh ON owh.orderid = o.orderid " +
+                        "full JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "full JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId")
+        public Double findAverageStarsByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 房東所有工作總評價數 */
+        @Query("SELECT count(r.reviewid)  FROM OrderBean o " +
+                        "full JOIN ReviewBean r ON r.orderid = o.orderid " +
+                        "full JOIN OrderWorkHouseBean owh ON owh.orderid = o.orderid " +
+                        "full JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "full JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId")
+        public Integer findCountReviewByLandlordId(@Param("landlordId") Integer landlordId);
+
+        /** 房東每個工作分別評價數及評分 */
+        @Query("SELECT w.workid, count(r.reviewid), AVG(r.stars)  FROM OrderBean o " +
+                        "full JOIN ReviewBean r ON r.orderid = o.orderid " +
+                        "full JOIN OrderWorkHouseBean owh ON owh.orderid = o.orderid " +
+                        "full JOIN WorkHouseBean wh ON owh.workhouseid = wh.id " +
+                        "full JOIN WorkBean w ON w.workid = wh.workid " +
+                        "WHERE w.landlordid = :landlordId GROUP BY w.workid")
+        public List<Object[]> findWorkReviewByLandlordId(@Param("landlordId") Integer landlordId);
 
 }
-
